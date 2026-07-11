77a94ec677385e3b8ff03790417e7c52664035b4
Chinthak Divan K <149614220+Chinthak123@users.noreply.github.com>
2026-07-11 22:31:08 +0530

    Reformat main.py with consistent indentation

--- main.py
+++ main.py
@@ -4,21 +4,17 @@ from pydantic import BaseModel
 from pathlib import Path
 import pickle
 import numpy as np
-
 app = FastAPI()
 base_dir = Path(__file__).resolve().parent
 
 model = pickle.load(open('model.pkl', 'rb')) 
 
-
 class PredictionRequest(BaseModel):
     rainfall: float
     temp: float
-
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     return FileResponse(base_dir / "index.html", media_type="text/html")
-
 @app.post("/predict")
 async def get_prediction(data: PredictionRequest):
     
